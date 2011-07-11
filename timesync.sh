144280f5d197077fa76e7fb5bacb6e32064c3cd5
Christian Busch <chris@debilux.org>
2011-07-11 10:22:15 +0200

    ### 2011-07-11
    * Did some code cleanup
    * If the target directory on the server doesn't exist, we now try to create it
    * Now a folder for every client is created in the backup directory
    * rsync is now running as root
    * Put some documentation in the code and startet the README and changelog

--- timesync.sh
+++ timesync.sh
@@ -2,36 +2,63 @@
 
 set -e 
 
-SOURCES="/Library /System /bin /etc /mach_kernel /private /sbin /tmp /usr /var /etc /opt /Volumes/Macintosh/Library /Volumes/Macintosh/System /Volumes/Macintosh/Users /Volumes/Macintosh/Applications"
+## Which folders do you want to Backup? (Without trailing "/")
+# SOURCES="/Library /System /bin /etc /mach_kernel /private /sbin /tmp /usr /var /etc /opt /Volumes/Macintosh/Library /Volumes/Macintosh/System /Volumes/Macintosh/Users /Volumes/Macintosh/Applications"
+SOURCES="/Volumes/Macintosh/Users/chris"
 
+## Your server login ("username@server")
+SSH_USER="chris@baader"
+
+## What's your path for Backups on your Server? (i.e. "/home/username/backups"
+SNAPSHOT_DIR="/volume1/homes/chris/Backup"
+
+## Path to your rsync exclude file
+EXCLUDE_FILE="/Users/chris/bin/backup_excludes.txt"
+
+## The path to your rsync binary (usually "/usr/bin/rsync")
+RSYNC="/usr/local/bin/rsync"
+
+
+### DO NOT EDIT BELOW THIS LINE ###
+
+## Get the hostname of the client
 SRCHOST=`hostname -fs`
-snapshot_host=baader
-snapshot_user=chris
-snapshot_dir=/volume1/homes/chris/Backup/$SRCHOST
-ssh_user=$snapshot_user@$snapshot_host
 
-ping -o $snapshot_host > /dev/null || {
-  echo "WARNING: can't see $snapshot_host -- skipping backup"
+## Add cleint name to the backup path
+SNAPSHOT_DIR="${SNAPSHOT_DIR}/${SRCHOST}/"
+
+## Split SSH_USER in username and host
+SNAPSHOT_USER=${SSH_USER%@*}
+SNAPSHOT_HOST=${SSH_USER#*@}
+
+## The name for the snapshots
+SNAPSHOT_ID=`date "+%Y-%m-%d-%H%M%S"`
+
+# Is your server online?
+ping -o $SNAPSHOT_HOST > /dev/null || {
+  echo "WARNING: can't see $SNAPSHOT_HOST -- skipping backup"
   exit 1
 }
 
-ssh $ssh_user "test -d $snapshot_dir" || {
-  ssh $ssh_user "mkdir -p $snapshot_dir" || {
-  	echo "ERROR: can't see $ssh_user:$snapshot_dir" >&2
+# Are we able to create and write to the backup folder?
+ssh $SSH_USER "test -d $SNAPSHOT_DIR" || {
+  ssh $SSH_USER "mkdir -p $SNAPSHOT_DIR" || {
+  	echo "ERROR: can't see $SSH_USER:$SNAPSHOT_DIR" >&2
   	exit 2
   }
 }
-  
-snapshot_id=`date "+%Y-%m-%d-%H%M%S"`
 
-/usr/bin/rsync --archive --verbose \
+## Do it!
+sudo $RSYNC -v --archive --verbose \
   --delete --delete-excluded \
+  --exclude-from $EXCLUDE_FILE \
   --numeric-ids \
   --one-file-system \
   --partial \
   --link-dest ../current/ \
   --relative \
   $SOURCES \
-  $ssh_user:$snapshot_dir/in-progress/
-
-ssh $ssh_user "cd $snapshot_dir; rm -fr $snapshot_id; mv in-progress $snapshot_id; rm -f current; ln -s $snapshot_id $snapshot_dir/current"
+  $SSH_USER:$SNAPSHOT_DIR/in-progress/
+  
+## Finish backup and get some cleaning done
+ssh $SSH_USER "cd $SNAPSHOT_DIR; rm -fr $SNAPSHOT_ID; mv in-progress $SNAPSHOT_ID; rm -f current; ln -s $SNAPSHOT_ID $SNAPSHOT_DIR/current"
